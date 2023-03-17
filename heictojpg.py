2ed9331ec3d9dfb5a35f349b385a5fa34f032cbd
whllm <admin@whllm.net>
2023-03-17 16:27:50 -0700

    Fixed filename issue and added output directory check

--- heictojpg.py
+++ heictojpg.py
@@ -3,11 +3,14 @@
 
 import os, subprocess
 
-directory = '.'
-sub = '.\out'
+working_dir = '.'
+output_dir = 'output'
 
-for filename in os.listdir(directory):
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+for filename in os.listdir(working_dir):
     if filename.lower().endswith(".heic"): 
-        print('Converting %s...' % os.path.join(directory, filename))
-        subprocess.run(["magick", "%s" % filename, "%s" % (os.path.join(sub, directory) + filename[0:-5] + '.jpg')])
+        print('Converting %s...' % os.path.join(working_dir, filename))
+        subprocess.run(["magick", "%s" % filename, "%s" % (os.path.join(output_dir, filename[0:-5]) + '.jpg')])
         continue
